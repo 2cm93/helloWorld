0db8cbda0bc07889ab904bb0b115988d18c3896e
2cm93 <dlcksal93@gmail.com>
2017-04-26 19:54:33 +0900

    Create 10953.cpp

---
+++ 10953.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	int a = 0, b = 0;
+	char c;
+	int t = 0;
+	cin >> t;
+	while (t--) {
+		cin >> a >>c >> b;
+		
+		cout << a + b << endl;
+	}
+
+	return 0;
+}
